aef49928c3c368c5bd699c00f40ceeb484e7b79d
jason1_su <jason1_su@askey.com>
2019-12-15 00:18:47 +0800

    implement BaseModel

--- androidlib/src/main/java/crop/computer/askey/androidlib/mvp/BaseModel.java
+++ androidlib/src/main/java/crop/computer/askey/androidlib/mvp/BaseModel.java
@@ -1,4 +1,11 @@
 package crop.computer.askey.androidlib.mvp;
 
 public interface BaseModel {
+    void registerCallback(Callback callback);
+    void unregister(Callback callback);
+
+    interface Callback {
+        void onSuccess(Object data);
+        void onFail(String errorType, String errorMessage);
+    }
 }
