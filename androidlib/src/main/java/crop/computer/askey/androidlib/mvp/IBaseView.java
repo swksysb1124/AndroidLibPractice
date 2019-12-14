afbf699b030807a6dbdeafaec7b1d422f33afab5
jason1_su <jason1_su@askey.com>
2019-12-15 00:38:49 +0800

    add more methods in IBaseView

--- androidlib/src/main/java/crop/computer/askey/androidlib/mvp/IBaseView.java
+++ androidlib/src/main/java/crop/computer/askey/androidlib/mvp/IBaseView.java
@@ -6,4 +6,11 @@ package crop.computer.askey.androidlib.mvp;
 
 public interface IBaseView<T> {
     void setPresenter(T presenter);
+    void showProgressBar(Object pbInfo);
+    void updateProgress(Object progress);
+    void hideProgressBar(Object pbInfo);
+    void showMessage(Object msgInfo);
+    void hideMessage(Object msgInfo);
+    void goToPage(Object pageInfo);
+    void runOnUiThread(Runnable task);
 }
